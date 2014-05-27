cdd5366a004b394ddddb5b7937dc720046946487
Amanpreet Singh <apsdehal@gmail.com>
2014-05-27 23:44:33 +0530

    Adds functionality of calling functions

--- src/Link.php
+++ src/Link.php
@@ -5,11 +5,27 @@ class Link {
 	public static function all( $routes ) {
 		$method = strtolower($_SERVER['REQUEST_METHOD']);
 		$path = '/';
-		if( !empty ( $_SERVER['PATH_INFO'] ) ) {
+		if ( !empty ( $_SERVER['PATH_INFO'] ) ) {
 			$path = $_SERVER['PATH_INFO'];
 		} else if ( !empty ( $_SERVER['REQUEST_URI'] ) ) {
             $path = (strpos($_SERVER['REQUEST_URI'], '?') > 0) ? strstr($_SERVER['REQUEST_URI'], '?', true) : $_SERVER['REQUEST_URI'];
 		}
-		echo $path;
+
+		if ( isset($routes[$path] ) ) {
+			$handler = $routes[$path]
+		}
+
+		if ( isset($handler) ) {
+			if ( is_callable($handler) ){
+				$instanceOfHandler = $handler();
+			} else {
+				$instanceOfHandler = new $handler();
+			}
+		}
+
+		if( isset( $handler ) ){
+			if( method_exists( $handler, $method ) )
+				call_user_func(array($handler, $method);
+		}
 	}
 }
