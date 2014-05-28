58aa7568d6e6ecaba602cecfee6281bba40b0854
Amanpreet Singh <apsdehal@gmail.com>
2014-05-28 13:01:03 +0530

    Adds regex check for params, now you can pass parameters

--- src/Link.php
+++ src/Link.php
@@ -7,6 +7,8 @@ class Link {
 	public static function all( $routes ) {
 		$method = strtolower($_SERVER['REQUEST_METHOD']);
 		$path = '/';
+		$handler = null;
+		$matched = array();
 		if ( !empty ( $_SERVER['PATH_INFO'] ) ) {
 			$path = $_SERVER['PATH_INFO'];
 		} else if ( !empty ( $_SERVER['REQUEST_URI'] ) ) {
@@ -14,23 +16,46 @@ class Link {
 		}
 
 		if ( isset($routes[$path] ) ) {
-			$handler = $routes[$path]
+			$handler = $routes[$path];
 		}
 
-		if ( isset($handler) ) {
+		if ( $handler ) {
 			if ( is_callable($handler) ){
 				$instanceOfHandler = $handler();
 			} else {
 				$instanceOfHandler = new $handler();
 			}
+		} else if ( $routes ) {
+			
+			$regex = array(
+				'/{i}/',
+				'/{s}/',
+				'/{a}/'
+				);
+
+			$replacements = array(
+				'([\d]+)'	,
+				'([a-zA-Z]+)',
+				'([\w-]+)'
+				);
+
+			foreach ( $routes as $routePath => $routeHandler ){
+				$routePath = preg_replace( $regex, $replacements, $routePath );
+				if( preg_match( '#^/?' . $routePath . '/?$#', $path, $matches ) ){
+					$handler = $routeHandler;
+					$matched = $matches;
+					var_dump($matched);
+					break;
+				}
+			}
 		}
 
-		if( isset( $handler ) ) {
-			if( method_exists( $handler, $method ) ) {
+		if( isset( $instanceOfHandler ) ) {
+			if( method_exists( $instanceOfHandler, $method ) ) {
 				try {
-					call_user_func(array($handler, $method));
+					call_user_func_array( array( $instanceOfHandler, $method ), $matched );
 					if ( !isset( self::$recognized[$path]  ) ) {
-						self::$recognized[$path] = $handler;
+						self::$recognized[$path] = $instanceOfHandler;
 					}
 				} catch ( Exception $e ){
 					echo $e;
