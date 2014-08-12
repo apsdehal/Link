0ef7a2faed54f02b0e0ad12281f33fd3428d8dfe
Amanpreet Singh <apsdehal@gmail.com>
2014-08-12 17:00:19 +0530

    Shifts Link to PS standards

--- src/Link.php
+++ src/Link.php
@@ -3,7 +3,8 @@
 /**
  * @class Main class of the Link router that helps you create and deploy routes
  */	
-class Link {
+class Link 
+{
 
 	/**
 	 * @var array A collection of the routes originally passed into all function. Used by static function route
@@ -25,10 +26,11 @@ class Link {
 	 * 
 	 * @param array $routes An array of combination of the path and its handler, that are final deployed for a particular url
 	 */	
-	public static function all( $routes ) {
+	public static function all( $routes ) 
+	{
 
 		/* Call all functions that are to be executed before routing */
-		foreach( self::$beforeFuncs as $beforeFunc ){
+		foreach( self::$beforeFuncs as $beforeFunc ) {
 			if( $beforeFunc[1] )
 				call_user_func_array( $beforeFunc[0] , $beforeFunc[1] );
 			else
@@ -108,7 +110,8 @@ class Link {
 	 * @param array $params An array of parameters that are replaced in the route if it contains wildcards
 	 *						For e.g. if route is /name/{i}/{a} and parameters passed are 1, aps then link generated will be /name/1/aps  
 	 */	
-	public static function route( $name, $params = array() ){
+	public static function route( $name, $params = array() )
+	{
 		$href = null;
 		foreach ( self::$routes as $routePath => $routeDesc ) {
 			if( is_array( $routeDesc ) ){
@@ -127,7 +130,8 @@ class Link {
 	 * Static function to handle cases when route is not found, call handler of 404 if defined else
 	 * sends a 404 header
 	 */	
-	public static function handle404() {
+	public static function handle404() 
+	{
 		/* Call '404' route if it exists */
 		if( isset ( self::$routes['404'] ) )
 			call_user_func( self::$routes['404'] );
@@ -144,9 +148,10 @@ class Link {
 	 * 							the wildcards that were originally passed, this newParams will 
 	 *							be next passed to main handler   
 	 */	
-	public static function callFunction( $handler , $matched, $method ){
+	public static function callFunction( $handler , $matched, $method ) 
+	{
 		if ( $handler ) {	
-			if ( is_callable( $handler ) ){
+			if ( is_callable( $handler ) ) {
 				$newParams = call_user_func_array( $handler, $matched ) ;
 			} else {
 		
@@ -188,7 +193,8 @@ class Link {
 	 * @param array $params Array of parameters that are to be passed to before function, can be null but if not
 	 *						it must be an array 
 	 */	
-	public static function before( $funcName, $params = null ){
+	public static function before( $funcName, $params = null )
+	{
 		array_push( self::$beforeFuncs, [ $funcName, $params ]);
 	}
 
@@ -199,7 +205,8 @@ class Link {
 	 * @param array $params Array of parameters that are to be passed to after function, can be null but if not
 	 *						it must be an array 
 	 */	
-	public static function after( $funcName, $params = null ){
+	public static function after( $funcName, $params = null )
+	{
 		array_push( self::$afterFuncs, [ $funcName, $params ]);
 	}
 }
